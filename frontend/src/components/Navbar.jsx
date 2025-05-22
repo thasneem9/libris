03084767295417a3a183660f296c805dbba666e2
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-05-22 20:23:35 +0530

    lights

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { FaSearch } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import './navbar.css';
-
+import lights from '../images/lights.png'
 const Navbar = () => {
+   
   return (
+    <>
+   
   <div className="navbar">
+    
   <h1 className="navbar-title">Libris</h1>
 
   <div className="navbar-right">
@@ -20,7 +24,58 @@ const Navbar = () => {
     <GiHamburgerMenu size={30} className="menu-icon" />
   </div>
 </div>
+{/*  <div className="lights-container">
+      <img src={lights} className="lights"></img>
+      <img src={lights} className="lights"></img>
+    
+    </div>  */}
+  <ul className="lightrope">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
 
+     
+</>
   );
 };
 
