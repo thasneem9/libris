38a2c9f5a61f6898300f2053e0b7dbe476de883c
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-05-28 13:13:53 +0530

    buton color clasname persistance when clicked

--- frontend/src/components/Robot.jsx
+++ frontend/src/components/Robot.jsx
@@ -4,8 +4,12 @@ import { useState } from 'react'
 import './robot.css'
 import { IoBookOutline } from "react-icons/io5";
 
+
 const Robot = () => {
     const [settings, setSettings]=useState(false);
+  
+    const [rightCol, setRightCol]=useState('account')
+
   return (
     <>
     <div className="robot-wrapper" >
@@ -24,21 +28,54 @@ const Robot = () => {
 
         <div className='popup-columns'>
           <div className='col-1'>
-            <button>Add Book</button>
-            <button>Qoutes</button>
-            <button>Highlights</button>
-            <button>Vocab Learnt Book</button>
-            <button>Time Spent</button>
-            <button>Account</button>
+            <button onClick={()=>setRightCol('account')} className={rightCol==='account'?'clicked':''}>Account</button>
+            <button onClick={()=>setRightCol('addBook')} className={rightCol==='addBook'?'clicked':''}>Add Book</button>
+            <button onClick={()=>setRightCol('quotes')} className={rightCol==='quotes'?'clicked':''}>Qoutes</button>
+            <button onClick={()=>setRightCol('highlights')} className={rightCol==='highlights'?'clicked':''}>Highlights</button>
+            <button onClick={()=>setRightCol('vocabBox')} className={rightCol==='vocabBox'?'clicked':''}>Vocab Learnt Box</button>
+            <button onClick={()=>setRightCol('timeStats')} className={rightCol==='timeStats'?'clicked':''}>Time Spent</button>
+          
           </div>
           <div className='col-2'>
-            <div className='inner-box'>
+           {rightCol==='account' && (
+            <>
+            
+              
+            </>
+           )}
+           {rightCol==='addBook' && (
+            <>
+             <div className='inner-box'>
              <IoBookOutline  size={300} className='book-icon'/>
 
               <p>Drag and Drop a PDF File into this Box</p>
               <button>Choose A File</button>
             </div>
             <button>Import Book</button>
+            </>
+           )}
+           {rightCol==='quotes' && (
+            <>
+             <h1>Qoutes box</h1>
+            </>
+           )}
+           {rightCol==='highlights' && (
+            <>
+             <h1></h1>
+            </>
+           )}
+           
+           {rightCol==='VocabBox' && (
+            <>
+             <h1>vocab</h1>
+            </>
+           )}
+           {rightCol==='timeStats' && (
+            <>
+             <h1></h1>
+            </>
+           )}
+          
           </div>
         </div>
       </div>
